bec86c2e47f78a5949f061c0d64ca4f2f8d1521d
Anisha <anisha.0612@live.com>
2020-10-08 13:19:25 -0400

    adding progress bar and duration function

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const image = document.querySelector("img");
 const title = document.getElementById("title");
 const artist = document.getElementById("artist");
 const music = document.querySelector("audio");
+const progressContainer = document.querySelector(".progress-container");
+const progress = document.getElementById("progress");
+const currentTimeEle = document.getElementById("current-time");
+const durationEle = document.getElementById("duration");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
@@ -11,6 +15,9 @@ let isPlaying = false;
 
 // to store top 10 tracks
 let songs = [];
+let genre = {
+  Hits: {},
+};
 
 // play song
 const playSong = () => {
@@ -37,12 +44,12 @@ function loadSong(song) {
 }
 
 let songIndex = 0;
+
 function prevSong() {
   songIndex--;
   if (songIndex < 0) {
     songIndex = songs.length - 1;
   }
-  console.log(songIndex);
   loadSong(songs[songIndex]);
   playSong();
 }
@@ -52,23 +59,25 @@ function nextSong() {
   if (songIndex > songs.length - 1) {
     songIndex = 0;
   }
-  console.log(songIndex);
   loadSong(songs[songIndex]);
   playSong();
 }
 
 const apiCall = () => {
   const corsUrl = "https://cors-anywhere.herokuapp.com/";
-  const apiUrl = "https://api.deezer.com/chart/0/tracks";
+  const topTracksUrl = "https://api.deezer.com/chart/0/tracks";
+  const playlistUrl = "https://api.deezer.com/radio/37151/tracks";
+  const genreUrl = "https://api.deezer.com/radio";
+
   axios
-    .get(`${corsUrl + apiUrl}`)
+    .get(`${corsUrl + playlistUrl}`)
     .then((response) => {
-      //   console.log(response.data.data);
+      // console.log(response.data.data);
       songs = response.data.data;
       console.log(songs);
-      return response.data.data;
+      loadSong(songs[songIndex]);
     })
-    .then((result) => loadSong(result[4]))
+
     .catch((err) => console.log(err));
 };
 
@@ -78,5 +87,36 @@ playBtn.addEventListener("click", () => {
   isPlaying ? pauseSong() : playSong();
 });
 
+function updateProgressBar(e) {
+  if (isPlaying) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+    // duration calculation
+    const durationSeconds = Math.floor(duration);
+    if (durationSeconds) {
+      durationEle.textContent = `0:${durationSeconds}`;
+    }
+    // current time calculation
+    let currentTimeSeconds = Math.floor(currentTime);
+    if (currentTimeSeconds < 10) {
+      currentTimeSeconds = `0${currentTimeSeconds}`;
+    }
+    if (currentTimeSeconds) {
+      currentTimeEle.textContent = `0:${currentTimeSeconds}`;
+    }
+  }
+}
+
+function setProgressBar(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const { duration } = music;
+  music.currentTime = (clickX / width) * duration;
+}
+
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
+music.addEventListener("ended", nextSong);
+music.addEventListener("timeupdate", updateProgressBar);
+progressContainer.addEventListener("click", setProgressBar);
